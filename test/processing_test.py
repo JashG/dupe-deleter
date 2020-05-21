7e56749b19df4db51e95e47dfe299a7f06d69980
JashG <jashgulabrai98@gmail.com>
2020-05-21 16:27:48 -0400

    Add tests for recursive delete

--- test/processing_test.py
+++ test/processing_test.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import shutil
 import processing as pro
 
 # File paths/directories created in tests
@@ -13,29 +14,14 @@ class MyTestCase(unittest.TestCase):
         """
         Populate BASE_PATH directory with files, some of which are duplicates.
         """
-        # Create test directory and populate with files (some duplicates)
-        try:
-            os.makedirs(BASE_PATH, exist_ok=True)
-        except OSError:
-            print("Setup failed with path: %s" % BASE_PATH)
+        cls.__setup_directory(cls, recursive=True)
 
-        # Create 50 files; create 1 duplicate for every other file; create additional duplicate
-        # for every tenth file
-        for i in range(1, 51):
-            file_name = BASE_PATH + '/random_' + str(i) + '.txt'
-            contents = str(i) + ' contents'
-            with open(file_name, 'w') as fp:
-                fp.write(contents)
-            # Create duplicate for every other file
-            if i % 2 == 0:
-                file_name = BASE_PATH + '/random_' + str(i) + '_2.txt'
-                with open(file_name, 'w') as fp:
-                    fp.write(contents)
-                # Create additional duplicate for every third file
-                if i % 10 == 0:
-                    file_name = BASE_PATH + '/random_' + str(i) + '_3.txt'
-                    with open(file_name, 'w') as fp:
-                        fp.write(contents)
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Remove all files created in BASE_PATH directory.
+        """
+        cls.__teardown_directory()
 
     def test_check_duplicates(self):
         """
@@ -57,6 +43,25 @@ class MyTestCase(unittest.TestCase):
             for file in dupes[key]:
                 self.assertEqual(base_name, self.__get_file_base_name(file))
 
+        # Now, check the directory recursively
+        dupes = pro.check_duplicates(BASE_PATH, recursive=True)
+
+        for key in dupes:
+            files = dupes[key]
+
+            if len(files) == 0:
+                continue
+            if len(files) == 1:
+                self.fail("Only one duplicate was reported for key: ", key)
+
+            base_name = self.__get_file_base_name(files[0])
+
+            for file in dupes[key]:
+                self.assertEqual(base_name, self.__get_file_base_name(file))
+
+    # # # # # # # # # # #
+    # Helper functions  #
+    # # # # # # # # # # #
     @staticmethod
     def __get_file_base_name(path):
         parts = path.split('_')
@@ -64,6 +69,42 @@ class MyTestCase(unittest.TestCase):
             return parts[1].split('.')[0]
         if len(parts) == 3:
             return parts[1]
+
+    @staticmethod
+    def __setup_directory(cls, recursive=True):
+        cls.__create_test_directory()
+        if recursive:
+            cls.__create_test_directory('../test/files/folder1', extra=3, double_extra=5)
+
+    @staticmethod
+    def __create_test_directory(path=BASE_PATH, extra=2, double_extra=10):
+        # Create test directory and populate with files (some duplicates)
+        try:
+            os.makedirs(path, exist_ok=True)
+        except OSError:
+            print("Setup failed with path: %s" % path)
+
+        # Create 50 files; create 1 duplicate for every <extra> file; create additional duplicate
+        # for every <double_extra> file
+        for i in range(1, 51):
+            file_name = path + '/random_' + str(i) + '.txt'
+            contents = str(i) + ' contents'
+            with open(file_name, 'w') as fp:
+                fp.write(contents)
+
+            if i % extra == 0:
+                file_name = path + '/random_' + str(i) + '_2.txt'
+                with open(file_name, 'w') as fp:
+                    fp.write(contents)
+
+                if i % double_extra == 0:
+                    file_name = path + '/random_' + str(i) + '_3.txt'
+                    with open(file_name, 'w') as fp:
+                        fp.write(contents)
+
+    @staticmethod
+    def __teardown_directory():
+        shutil.rmtree(BASE_PATH)
 
 
 if __name__ == '__main__':
